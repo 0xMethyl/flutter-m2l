ba55f7a32b9f122d12d6d84e40de97d34b26e7a8
unknown <juliendubois02@yahoo.fr>
2022-06-01 10:55:35 +0200

    BottomNavBar

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/pages/Affichage.dart';
+import 'package:flutterapp/pages/LoginPage.dart';
+import 'package:flutterapp/produit.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'connection flutter et nodeJS via express',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      routes: {
+        '/' : (context) => LoginPage(),
+        '/home': (context) => Affichage()
+      },
+    );
+  }
+}
